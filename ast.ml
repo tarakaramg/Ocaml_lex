ad9f4d4abf96d392d5072c68f5749b6347f319e6
R <gtr@brandeis.edu>
2020-07-02 15:40:51 -0400

    test

---
+++ ast.ml
@@ -0,0 +1,13 @@
+type bop =
+| Add
+| Mult
+| Leq
+
+type expr = 
+| Var of string
+| Int of int
+| Bool of bool
+| Binop of bop * expr * expr
+| Let of string * expr * expr
+| If of expr * expr * expr
+
